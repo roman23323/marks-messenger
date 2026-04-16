38c642ed43df040a68977eb36fb0e9c5c7fad66c
roman23323 <ignatenko_roman.s@mail.ru>
2026-04-16 17:26:11 +0300

    feat: register ConfigModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebSocketModule } from './web-socket/web-socket.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WebSocketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validate: (env) => ({
+        GIGACHAT_AUTH_KEY: env.GIGACHAT_AUTH_KEY
+      })
+    }),
+    WebSocketModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
