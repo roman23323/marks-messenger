6273e6e9030ce351610b24d5d81ce81fff353b82
roman23323 <ignatenko_roman.s@mail.ru>
2026-04-16 14:21:12 +0300

    feat: add WS module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebSocketModule } from './web-socket/web-socket.module';
 
 @Module({
-  imports: [],
+  imports: [WebSocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
