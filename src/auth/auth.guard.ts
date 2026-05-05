83464b7372467d7802a1168898894ff405dbcede
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-05 22:01:53 +0300

    fix: mark AuthGuard with @Injectable

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,8 +1,9 @@
-import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Observable } from "rxjs";
 import { Request } from "express"
 
+@Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly jwtService: JwtService) {}
 
