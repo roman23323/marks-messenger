879156c255a40b0416faffcf40faf72d0552aec0
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-06 22:36:19 +0300

    fix(auth): add await to verifyAsync

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,20 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { Observable } from "rxjs";
 import { Request } from "express"
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly jwtService: JwtService) {}
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
         if (!token) {
             throw new UnauthorizedException('Требуется авторизация');
         }
         try {
-            const payload = this.jwtService.verifyAsync(token);
+            const payload = await this.jwtService.verifyAsync<{ sub: number }>(token);
             request['user'] = payload;
         } catch (e) {
             throw new UnauthorizedException(`Авторизация не удалась: ${e.message}`);
