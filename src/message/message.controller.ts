25e45e0242a64dc28273486ecfa6fbc8494680f9
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-06 22:44:19 +0300

    feat: protect send-message route

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import { SendMessageDto } from './dto/send-message.dto';
 import { MessageService } from './message.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
+  @UseGuards(AuthGuard)
   @Post('send-message')
   @HttpCode(202)
   async sendMessage(
-    @Body() message: SendMessageDto,
+    @Body() sendMessageDto: SendMessageDto,
+    @Req() request
   ) {
     console.log('Поступило сообщение!');
     const jobData = await this.messageService.processMessage(message);
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { BullModule } from '@nestjs/bullmq';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'messages',
     }),
+    AuthModule
   ],
   providers: [MessageService],
   controllers: [MessageController]
