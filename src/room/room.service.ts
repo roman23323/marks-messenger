89ccb3d8831fc6b24cad5253ef6be71bea0517a7
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-06 22:58:12 +0300

    feat: add create room method

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RoomService {}
+export class RoomService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createRoom(name: string) {
+        const room = await this.prisma.room.create({
+            data: {
+                name
+            }
+        });
+        return room.id;
+    }
+}
