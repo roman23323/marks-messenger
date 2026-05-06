a19071287357a2c0a01fe674874ccb4f8b4732e8
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-06 22:57:06 +0300

    feat: protect Sse route

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, MessageEvent, Param, Sse } from '@nestjs/common';
 import { Observable, finalize } from 'rxjs';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { SseRegistryService } from 'src/events/events.service';
 
 @Controller('room')
 export class RoomController {
   constructor(private readonly sseRegistry: SseRegistryService) {}
 
+  @UseGuards(AuthGuard)
   @Sse(':roomId')
   streamEvents(@Param('roomId') roomId: string): Observable<MessageEvent> {
     console.log('Подключение к комнате по id: ', roomId);
