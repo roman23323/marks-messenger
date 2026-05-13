04f5f3f4ef8f7ac5d2f32126b9ce45346e5b8ffd
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-13 23:11:23 +0300

    refactor: clear imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { Body, Controller, Get, HttpCode, Post, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { SendMessageDto } from './message/dto/send-message.dto';
-import { MessageService } from './message/message.service';
 
 @Controller()
 export class AppController {
