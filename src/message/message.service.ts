a707ff550a0128c90cf660a7dd835d133b5f24a9
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-14 12:43:13 +0300

    feat: add user in room validation

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,9 +1,11 @@
 import { InjectQueue } from '@nestjs/bullmq';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { Queue } from 'bullmq';
 import { SendMessageDto } from './dto/send-message.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 export interface Message extends SendMessageDto {
+    id: string,
     userId: number
 }
 
@@ -11,14 +13,26 @@ export interface Message extends SendMessageDto {
 export class MessageService {
     constructor(
         @InjectQueue('messages')
-        private readonly messageQueue: Queue<Message>,
+        private readonly messageQueue: Queue<{ roomId: string, userId: number, text: string }>,
+        private readonly prisma: PrismaService
     ) {}
 
-    async processMessage(message: Message) {
+    async processMessage(roomId: string, userId: number, text: string) {
+        const userToRoom = await this.prisma.usersToRooms.findFirst({
+            where: {
+                roomId,
+                userId
+            }
+        });
+
+        if (!userToRoom) {
+            throw new ForbiddenException('Вы не состоите в данной комнате');
+        }
+
         try {
             const job = await this.messageQueue.add(
                 'send-message',
-                message,
+                { roomId, userId, text },
                 {
                     removeOnComplete: true,
                 },
