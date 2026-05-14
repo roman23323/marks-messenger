f13dcb0b2bdf022158231b20afb73806e989cd2b
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-14 12:51:33 +0300

    feat: import jobs module

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -3,9 +3,10 @@ import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 import { EventsModule } from '../events/events.module';
 import { AuthModule } from '../auth/auth.module';
+import { JobsModule } from '../jobs/jobs.module';
 
 @Module({
-  imports: [EventsModule, AuthModule],
+  imports: [EventsModule, AuthModule, JobsModule],
   controllers: [RoomController],
   providers: [RoomService],
 })
