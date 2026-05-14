f418ea636ca68ebd86ee9709089a9bfc7a5a00c5
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-14 13:15:14 +0300

    feat: add room data management logic

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+    private readonly redis: Redis;
+
+    constructor(private readonly configModule: ConfigService) {
+        this.redis = new Redis({
+            host: configModule.get<string>('REDIS_URL'),
+            port: configModule.get<number>('REDIS_PORT')
+        })
+    }
+
+    async addUserToRoom(roomId: string, userId: number) {
+        await this.redis.sadd(`room:${roomId}`, userId);
+    }
+
+    async removeUserFromRoom(roomId: string, userId: number) {
+        await this.redis.srem(`room:${roomId}`, userId);
+    }
+
+    async getRoomMemers(roomId: string): Promise<string[]> {
+        return this.redis.smembers(`room:${roomId}`);
+    }
+}
