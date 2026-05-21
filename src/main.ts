082f38f8d212b5aec6db6fa00889400c5826a773
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-21 17:23:02 +0300

    fix: update path

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from 'nestjs-prisma';
-import { JsonLogger } from '../json-logger';
+import { JsonLogger } from './json-logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
